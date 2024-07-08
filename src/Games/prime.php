91685826384a58b7c76725756b68d1b36e2a5ecc
Roman <rnik82@mail.ru>
2024-07-09 00:33:16 +0300

    update condition of fn isPrime

--- src/Games/prime.php
+++ src/Games/prime.php
@@ -9,7 +9,7 @@ use function BrainGames\Cli\greet;
 function isPrime(int $number)
 {
     for ($i = 2; $i <= $number / 2; $i++) {
-        if ($number % $i === 0) {
+        if ($number % $i === 0 || $number < 2) {
             return false;
         }
     }
